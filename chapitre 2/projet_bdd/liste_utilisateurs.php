f93d407d60ae904e12481ea2d608f12450a143a2
mouadakz <abbad.mouadsolicode@gmail.com>
2026-02-24 10:28:21 +0100

    Add files via upload

---
+++ chapitre 2/projet_bdd/liste_utilisateurs.php
@@ -0,0 +1,23 @@
+<?php
+require 'connexion.php';
+
+try {
+    $sql = "SELECT * FROM Utilisateur";
+    $stmt = $pdo->query($sql);
+
+    $utilisateurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<table border='1'>
+<tr><th>ID</th><th>Nom</th><th>Email</th></tr>";
+foreach ($utilisateurs as $user) {
+    echo "<tr><td>{$user['id']}</td><td>{$user['nom']}</td><td>{$user['email']}</td></tr>";
+}
+echo "</table>";
+
+
+
+   
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+?>
